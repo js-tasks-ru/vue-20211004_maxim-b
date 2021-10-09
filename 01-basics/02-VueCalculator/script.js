000ac4f209dc552973fc1d0a0ca35c4af133bf94
maxim.b <maxim.b@intensa.ru>
2021-10-09 10:51:23 +0300

    done

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+ import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      multiplicand: 0,
+      factor: 0,
+      outcome: 0,
+      operation: undefined,
+      operations: {
+        sum: '+',
+        subtract: '-',
+        multiply: '*',
+        divide: '/'
+      }
+    }
+  },
+  watch: {
+    operation(NewValue) {
+            this.outcome = eval(this.multiplicand  + this.operations[NewValue] +  this.factor);
+    },
+    multiplicand(NewValue){
+      let operation  = this.operation;
+      this.outcome = eval(this.multiplicand  + this.operations[operation] +  this.factor);
+    },
+    factor(NewValue){
+      let operation = this.operation;
+      this.outcome = eval(this.multiplicand  + this.operations[operation] +  this.factor);
+    }
+  }
+}).mount('#app');
+
+window.vm = vm;
