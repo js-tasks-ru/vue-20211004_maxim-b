39e30a23e5dcc6fcc8b5a699e9a38200ed31c652
maxim.b <maxim.b@intensa.ru>
2021-10-13 14:00:21 +0300

    start MeetupView

---
+++ 02-components/06-MeetupView/MeetupCover.js
@@ -0,0 +1,19 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupCover',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: false
+    }
+  },
+  template: `
+    <div class="meetup-cover" :style="{ backgroundImage: 'url(' + image + ')' }">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
+    </div>`,
+});
---
+++ 02-components/06-MeetupView/MeetupInfo.js
@@ -0,0 +1,41 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+const options = {year: 'numeric', month: 'long', day: 'numeric' };
+
+export default defineComponent({
+  name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+      required: true
+    },
+    place: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    datetime() {
+      return new Date(this.date).toLocaleDateString('ru-RU', options)
+    }
+  },
+  template: `
+    <ul class="meetup-info">
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
+        {{ organizer }}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
+        {{ place }}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
+        <time datetime="datetime">{{ datetime }}</time>
+      </li>
+    </ul>`,
+});
