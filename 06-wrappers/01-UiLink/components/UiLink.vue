0a9579dade7f3bcd8809b5253c1ff5accdc10325
maxim.b <maxim.b@intensa.ru>
2021-11-02 23:31:11 +0300

    UiLink

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,10 +1,22 @@
 <template>
-  <a class="link">Link!</a>
+  <template v-if="this.$attrs.tag === 'router-link'">
+    <router-link class="link" :to="this.$attrs.to"><slot/></router-link>
+  </template>
+  <template v-else-if="this.$attrs.tag === 'a'">
+    <a class="link"><slot/></a>
+  </template>
+  <template v-else>
+    <router-link class="link" :to="this.$attrs.to"><slot/></router-link>
+  </template>
 </template>
 
 <script>
 export default {
   name: 'UiLink',
+  //inheritAttrs: false,
+  created() {
+    console.log(this.$attrs);
+  }
 };
 </script>
 
