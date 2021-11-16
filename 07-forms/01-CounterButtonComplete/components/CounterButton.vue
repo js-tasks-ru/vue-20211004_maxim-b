cef28faca24ddcb05a08152de8d4ec1dd25a6ea1
maxim.b <maxim.b@intensa.ru>
2021-11-16 09:00:26 +0300

    done CounterButtonComplete

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,39 @@
 <template>
-  <button></button>
+  <button @click="localCount++">{{ localCount }}</button>
 </template>
 
 <script>
+import { cloneDeep } from 'lodash-es';
 export default {
   name: 'CounterButton',
+  data () {
+    return {
+      localCount: null,
+    }
+  },
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+  emits: {
+    'update:count': null,
+  },
+  watch: {
+    count: {
+      deep: true,
+      immediate: true,
+      handler() {
+        this.localCount = cloneDeep(this.count);
+      },
+    },
+    localCount: {
+      deep: true,
+      handler(newValue) {
+        this.$emit('update:count', cloneDeep(newValue));
+      },
+    },
+  },
 };
 </script>
